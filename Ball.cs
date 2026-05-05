e28e52b1f57532a8b0a5cb3c4e940e1faa3a7eb6
Ajdin Kunosić <ajdin.kunosic@gmail.com>
2026-05-05 20:51:10 +0200

    Revert "[869cvt4n5] feat: Start match from play button"

--- Ball.cs
+++ Ball.cs
@@ -5,6 +5,12 @@ public partial class Ball : RigidBody2D
 	public override void _Ready()
 	{
 		AddToGroup("ball");
+
+		GravityScale = 0.0f;
+		LockRotation = true;
+		LinearDamp = 2.0f;
+		AngularDamp = 10.0f;
+		ContinuousCd = CCDMode.CastShape;
 	}
 
 	public void Kick(Vector2 direction, float strength)
